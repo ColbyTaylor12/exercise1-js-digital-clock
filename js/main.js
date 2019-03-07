aaf298ad1a8acdffc19546db34b98e04a6c93542
Colby <colbytaylor1212@gmail.com>
2019-03-06 21:31:25 -0500

    added more detailed comments to code

--- js/main.js
+++ js/main.js
@@ -5,10 +5,10 @@ $(document).ready( function() {
         var hours = currentTime.getHours();
         var minutes = currentTime.getMinutes();
         var seconds = currentTime.getSeconds();
-        // Let's set the AM and PM meridiem and 12-hour format
-        var meridiem = "AM"; // Default is AM 
+        // set tbe default meridiem
+        var meridiem = "AM"; 
 
-        //Convert from 24 hour to 12 hour format and keep track of the meridiem.
+        //Taking 12 from any value over 12 in tbe 24 hour clock and setting new meridiem
         if (hours > 12) {
             hours = hours - 12;
             meridiem = "PM";
@@ -19,9 +19,8 @@ $(document).ready( function() {
             hours = 12;
         }
 
-        // If the seconds digit is less than ten 
+        // If the seconds digit is less than ten Add the "0" digit to the front
         if (seconds < 10) {
-            // Add the "0" digit to the front so 9 becomes "09"
             seconds = "0" + seconds;
         }
 
@@ -35,7 +34,7 @@ $(document).ready( function() {
             hours = "0" + hours;
         }
 
-        // This gets a "handle" to the clock div in our HTML
+        // This connects the clock with the div ID clock in the bootstrap HTML
         var clockDiv = document.getElementById('clock');
 
         // Then we set the text inside the clock div to the hours, minutes, and seconds of the current time. 
